ed21d7fe8d39839970fcd890a21fd110d06eb085
ashleynw1800 <ashleynw1800@gmail.com>
2020-10-19 22:40:19 -0700

    animation-works-2

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -6,19 +6,12 @@ gsap.set("#big-circle",{transformOrigin:"center"});
 gsap.set("#med-circle",{transformOrigin:"center"});
 gsap.set("#small-circle",{transformOrigin:"center"});
 gsap.set("#dot",{transformOrigin:"center"});
-// gsap.set("#big-circle-cp",{transformOrigin:"center"});
-// gsap.set("#med-circle-cp",{transformOrigin:"center"});
-// gsap.set("#small-circle-cp",{transformOrigin:"right"});
 
 const loadTL = gsap.timeline();
 
 loadTL.from("#big-circle",{duration:1, alpha:0, ease:"back", scale:3, rotation:360},"1")
-    //.to("#big-circle-cp",{duration:3, rotation:-360},"2")
-    //.to("#big-circle-cp",{duration:1, alpha: 100, ease:"none", rotation:-720})
 
     .from("#med-circle",{duration:0.5, alpha:0, ease:"back", scale:0},"1")
-    // .to("#med-circle-cp",{duration:1, rotation:-360},"2")
-    // .to("#med-circle-cp",{duration:1, alpha: 80, ease:"none", rotation:-720})
 
     .from("#small-circle",{duration:0.5, alpha:0, ease:"back", scale:3, rotation:360},"1")
     
@@ -28,14 +21,16 @@ loadTL.from("#big-circle",{duration:1, alpha:0, ease:"back", scale:3, rotation:3
     .to("#med-circle",{duration:1, alpha:0, ease:"back", scale:0}, "3")
     .to("#small-circle",{duration:1, alpha:0, ease:"back", scale:3, rotation:360}, "3")
     .to("#dot",{duration:1.5, alpha:0, ease:"bounce", scale:0}, "3")
+
+    gsap.from("#color", {duration:1, alpha:0})
     
     gsap.from("#rect", {
-        duration: 0.7, 
+        duration: 0.5, 
         repeat: -1,
-        repeatDelay: 0.5,
+        repeatDelay: 0,
         alpha: 0,
         yoyo: true,
-        ease: "power1.inOut",
+        ease: "power1.out",
         motionPath:{
           path: "#path",
           align: "#path",
@@ -45,12 +40,12 @@ loadTL.from("#big-circle",{duration:1, alpha:0, ease:"back", scale:3, rotation:3
       },"-=4")
 
       gsap.to("#rect2", {
-        duration: 0.7, 
+        duration: 0.5, 
         repeat: -1,
-        repeatDelay: 0.5,
+        repeatDelay: 0,
         alpha: 0,
         yoyo: true,
-        ease: "power1.inOut",
+        ease: "power1.out",
         motionPath:{
           path: "#path2",
           align: "#path2",
@@ -60,12 +55,12 @@ loadTL.from("#big-circle",{duration:1, alpha:0, ease:"back", scale:3, rotation:3
       },"-=4")
       
       gsap.to("#rect3", {
-        duration: 0.7, 
+        duration: 0.5, 
         repeat: -1,
-        repeatDelay: 0.5,
+        repeatDelay: 0,
         alpha: 0,
         yoyo: true,
-        ease: "power1.inOut",
+        ease: "power1.out",
         motionPath:{
           path: "#path3",
           align: "#path3",
@@ -73,6 +68,9 @@ loadTL.from("#big-circle",{duration:1, alpha:0, ease:"back", scale:3, rotation:3
           alignOrigin: [0.5, 0.5]
         }
       },"-=4")
+
+    //   gsap.from("#body", {duration:1, alpha:0})
+    //   gsap.to("#body", {duration:1, alpha:0})
       ;
     
 
