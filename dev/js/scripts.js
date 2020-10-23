bfa547419cc2a4a207f99414ef42b11c0ade6a2e
ashleynw1800 <ashleynw1800@gmail.com>
2020-10-23 07:32:42 -0700

    dev-tools

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,7 +1,14 @@
 import gsap from "gsap/gsap-core"
 import {loadAnimation} from "./demo.js"
 //import {inAnimation} from "./demo.js"
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline({repeat:-1});
 mainTL.add(loadAnimation());
 //mainTL.add(inAnimation());
+
+
+//instantiate GSDevTools with default settings
+GSDevTools.create();
